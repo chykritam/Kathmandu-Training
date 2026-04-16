70e9d9f8b5a9a5983875281573b614ae9adccf60
Kritam <chykritam137@gmail.com>
2026-04-16 14:50:26 +0545

    day 5 loops

---
+++ Day 5/List.py
@@ -0,0 +1,49 @@
+print("This is new file for github branch testing")
+
+list1 = [1, 2, 3, 4, 5, 6, 7]
+print(list1)
+
+print(list1[-1])  # last element of the list
+print(list1[-2])  # second last element of the list
+print(list1[0:3])  # first three elements of the list
+print(list1[3:])  # all elements from index 3 to the end of the list
+print(list1[:4])  # all elements from the beginning of the list to index
+print(list1[::2])  # all elements from the list with a step of 2
+print(list1[1::2])  # all elements from the list with a step of 2 starting from index 1
+print(list1[::-1])  # all elements from the list in reverse order 
+print(list1[1:6:2])  # all elements from index 1 to index 5 with a step of 2
+print(list1[::3])  # all elements from the list with a step of 3
+print(list1[1:5:3])  # all elements from index 1 to index 4 with a step of 3
+print(list1[2:5:2])  # all elements from index 2 to index 4 with a step of 2
+print(list1[::4])  # all elements from the list with a step of 4
+print(list1[1:6:4])  # all elements from index 1 to
+print(list1[2:6:3])  # all elements from index 2 to index 5 with a step of 3
+print(list1[::5])  # all elements from the list with a step of 5
+print(list1[1:6:5])  # all elements from index 1 to
+print(list1[2:6:4])  # all elements from index 2 to index 5 with a step of 4
+
+
+
+
+
+list2 = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+print(list2)
+
+print(list2[-1])  # last element of the list
+print(list2[-2])  # second last element of the list 
+print(list2[0:3])  # first three elements of the list
+print(list2[3:])  # all elements from index 3 to the end of the list
+print(list2[:4])  # all elements from the beginning of the list to index 4
+print(list2[::2])  # all elements from the list with a step of  
+
+
+
+
+
+
+
+
+
+
+
+
